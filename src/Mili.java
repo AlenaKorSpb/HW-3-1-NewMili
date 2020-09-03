9d9203696176ccbcc6224f1f06250da1d36926f7
AlenaKorSpb <evtam-29@mail.ru>
2020-09-03 23:37:48 +0300

    added f.Mili

---
+++ src/Mili.java
@@ -0,0 +1,10 @@
+public class Mili {
+    public static void main(String[] args) {
+        int rub;
+        rub = 20;
+        long tiket = 1_000;
+        long mili = tiket / rub;
+
+        System.out.println(mili);
+    }
+}
